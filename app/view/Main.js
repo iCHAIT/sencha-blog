7ac312bc92c465d381cf2f2bcb04df0f40be0812
iCHAIT <cgupta319@gmail.com>
2014-12-20 19:59:12 +0530

    Added views

---
+++ app/view/Main.js
@@ -0,0 +1,28 @@
+Ext.define('GS.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+
+                xtype: 'homepanel'
+            },
+
+            {
+
+                xtype: 'blog'
+            },
+
+            {
+
+                xtype: 'contactform'
+            }
+        ]
+    }
+});
